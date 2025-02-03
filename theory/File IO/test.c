73e61c212cd119a6592b46212bf0376fde952ec8
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-02-03 21:33:08 +0100

    cosa troppo divertente

---
+++ theory/File IO/test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHAR 256
+int main() {
+    // Write C code here
+    char *filename = "stocazzo.txt";
+
+    FILE* fh_output;
+    fh_output = fopen(filename, "w");
+    
+    if ( fh_output ) printf("File: %s opened\n\n", filename);
+    else printf("File did not open\n");
+    
+    // how can i loop a fputs?
+    // if the new line string is equal to :wq -> quit
+    char buffer[MAX_CHAR];
+    while ( 1 )
+    {
+        printf("> ");  // Prompt for user input
+        if ( !fgets(buffer, MAX_CHAR, stdin) ) break;
+
+        // string complement span
+        // sostanzialmente che fa
+        // se l user va a capo, viene inserito \n
+        // con questa funzione lo prendiamo  e lo sostituiamo a \0 
+        // buffer[strcspn(buffer, "\n")] = '\0';
+
+        if ( strcmp(buffer, ":quit\n") == 0 ) break;
+
+        fputs(buffer, fh_output);
+    }
+
+    fclose(fh_output);
+
+    FILE* fh_input;
+    fh_input = fopen("stocazzo.txt", "r");
+
+    printf("\n");
+    while ( fgets(buffer, MAX_CHAR, fh_input) ) printf("%s", buffer);
+    printf("\n");
+
+    fclose(fh_input);
+    return 0;
+}
